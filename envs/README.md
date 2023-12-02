cc921271bc2966f44a47b749704fd627b5156f5f
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2023-12-03 00:35:44 +0300

    Refactor env

--- envs/README.md
+++ envs/README.md
@@ -1,6 +1,5 @@
 Настройки окружения хранятся здесь.
 
 * docker-compose.env - окружение docker compose
-* mongodb.env - окружение mongodb
-* development.env - окружение разработчика
-* production.env - окружение продакшена
+* development.env - окружение development
+* production.env - окружение production
--- project/project/settings.py
+++ project/project/settings.py
@@ -22,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS").split(" ")
 
 
 # Application definition
