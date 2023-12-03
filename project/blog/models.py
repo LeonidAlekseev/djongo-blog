22dffaa5ffdde4f47bbf5dd81ce62267e0203ff9
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2023-12-03 07:55:24 +0300

    Add makemigrations

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 
 class Post(models.Model):
-    title = models.CharField(primary_key=True, max_length=1000)
+    title = models.CharField(primary_key=True, max_length=64)
+    category = models.CharField(max_length=32, default='Без категории')
+    created_at = models.DateTimeField(default=datetime.now)
     body = models.TextField()
 
     def __str__(self):
