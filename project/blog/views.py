0ed52b18fd55e3df0046b5a43848d3101f9a6eb8
Lil Dojd <leonid.kores.alekseev.97@gmail.com>
2023-12-16 04:53:24 +0300

    Add site search

--- project/blog/views.py
+++ project/blog/views.py
@@ -4,10 +4,24 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'index.html'
 
+    def get_queryset(self):
+        search_post = self.request.GET.get('search')
 
+        if search_post:
+            queryset = Post.objects.filter(
+                models.Q(title__icontains=search_post) | \
+                models.Q(category__icontains=search_post) | \
+                models.Q(content__icontains=search_post),
+                status=1
+            ).order_by('-created_at')
+        else:
+            queryset = Post.objects.filter(status=1).order_by('-created_at')
+
+        return queryset
+
+models.Q
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post.html'
